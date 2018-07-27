33b22a7f5c1489577500feb4b9b81dea9e3ab839
Administrator <1822802785@qq.com>
2018-07-27 09:06:58 +0800

    添加App微信支付

---
+++ Magicodes.Pay/Magicodes.Pay.WeChat/Pay/Dto/AppPayOutput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magicodes.Pay.WeChat.Pay.Dto
+{
+    public class AppPayOutput
+    {
+        public string AppId { get; set; }
+
+        public string MchId { get; set; }
+
+        public string TimeStamp { get; set; }
+
+        public string NonceStr { get; set; }
+
+        public string PrepayId { get; set; }
+
+        public string SignType { get; set; }
+        public string PaySign { get; internal set; }
+    }
+}
--- Magicodes.Pay/Magicodes.Pay.WeChat/Pay/WeChatPayApi.cs
+++ Magicodes.Pay/Magicodes.Pay.WeChat/Pay/WeChatPayApi.cs
@@ -125,6 +125,103 @@ namespace Magicodes.Pay.WeChat
         }
 
         /// <summary>
+        /// APP支付
+        /// APP统一下单接口
+        /// https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_1
+        /// </summary>
+        /// <returns></returns>
+        public AppPayOutput AppPay(AppPayInput input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+            if (input.TotalFee <= 0)
+            {
+                throw new ArgumentException("金额不能小于0!", nameof(input.TotalFee));
+            }
+            if (string.IsNullOrWhiteSpace(input.Body))
+            {
+                throw new ArgumentNullException("商品描述必须填写!", nameof(input.Body));
+            }
+
+            if (string.IsNullOrWhiteSpace(input.SpbillCreateIp))
+            {
+                throw new ArgumentNullException("终端IP必须填写!", nameof(input.SpbillCreateIp));
+            }
+
+            var url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+
+            var config = GetPayConfigFunc();
+            if (config == null)
+            {
+                throw new ArgumentNullException("请配置支付配置信息!", "PayConfig");
+            }
+            if (string.IsNullOrWhiteSpace(config.PayAppId))
+            {
+                throw new ArgumentNullException("PayAppId必须配置!", nameof(config.PayAppId));
+            }
+            if (string.IsNullOrWhiteSpace(config.MchId))
+            {
+                throw new ArgumentNullException("MchId(商户Id)必须配置!", nameof(config.MchId));
+            }
+            if (string.IsNullOrWhiteSpace(config.PayNotifyUrl))
+            {
+                throw new ArgumentNullException("PayNotifyUrl(支付回调地址)必须配置!", nameof(config.PayNotifyUrl));
+            }
+            if (string.IsNullOrWhiteSpace(config.TenPayKey))
+            {
+                throw new ArgumentNullException("TenPayKey(支付密钥)必须配置!", nameof(config.TenPayKey));
+            }
+
+            var model = new AppUnifiedorderRequest
+            {
+                AppId = config.PayAppId,
+                MchId = config.MchId,
+                Attach = input.Attach,
+                Body = input.Body,
+                Detail = input.Detail,
+                FeeType = input.FeeType,
+                GoodsTag = input.GoodsTag,
+                LimitPay = input.LimitPay,
+                OutTradeNo = input.OutTradeNo ?? GenerateOutTradeNo(),
+                SpbillCreateIp = input.SpbillCreateIp,
+                TimeExpire = input.TimeExpire,
+                TimeStart = input.TimeStart,
+                TotalFee = ((int)(input.TotalFee * 100)).ToString(),
+            };
+            model.NonceStr = GetNoncestr();
+            model.NotifyUrl = config.PayNotifyUrl;
+            var dictionary = GetAuthors(model);
+            model.Sign = CreateMd5Sign(dictionary, config.TenPayKey); //生成Sign
+
+            var result = PostXML<UnifiedorderResult>(url, model);
+            if (result.IsSuccess())
+            {
+                var data = new
+                {
+                    appId = result.AppId,
+                    nonceStr = result.NonceStr,
+                    package = "prepay_id=" + result.PrepayId,
+                    signType = "MD5",
+                    timeStamp = GetTimestamp(),
+                };
+                return new AppPayOutput()
+                {
+                    AppId = data.appId,
+                    MchId = model.MchId,
+                    PrepayId = result.PrepayId,
+                    NonceStr = data.nonceStr,
+                    PaySign = model.Sign,
+                    SignType = data.signType,
+                    TimeStamp = data.timeStamp
+                };
+            }
+            LoggerAction("Error", "支付错误：" + result.GetFriendlyMessage());
+            throw new PayException("支付错误，请联系客服或重新支付！");
+        }
+
+        /// <summary>
         /// 支付回调通知处理
         /// </summary>
         /// <param name="inputStream"></param>
